318ef8674f8ebd6d2185118c9ae35bb9fee7a78c
Anthony Cardenas <logicainformatica18@gmail.com>
2024-03-19 20:06:36 -0500

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+     $task =Task::all();
+     return $task;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $task = new Task;
+
+        $task->title = $request->input('title');
+        $task->description = $request->input('description');
+        $task->save();
+
+        return response()->json([
+            'message' => 'Task created successfully',
+            'task' => $task
+        ]);
+    }
+    public function count()
+    {
+        return Task::all()->count();
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->all());
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return 204;
+    }
+}
